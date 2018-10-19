66d5c56aa2c392203c582a7317299ad3b1dd445c
DeValladolid <devalladolidoficial@gmail.com>
2018-10-19 10:42:03 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once 'fortnite-api/Autoloader.php';
 $api = new FortniteClient;
 $api->setKey('5ae9fa892ff9cf7b4bfb2cda585bc77a');
 
-$data = $api->items->store();
+$data = $api->challenges->get('current', 'en');
 
 var_dump($data);
 ?>
