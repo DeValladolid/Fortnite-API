865f390919733fdef4c0c17814b4eb5d2bf83b84
DeValladolid <devalladolidoficial@gmail.com>
2018-10-22 12:40:55 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,13 +3,16 @@
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game",
+  CURLOPT_URL => "https://fnbr.co/api/shop",
   CURLOPT_RETURNTRANSFER => true,
   CURLOPT_ENCODING => "",
   CURLOPT_MAXREDIRS => 10,
   CURLOPT_TIMEOUT => 30,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => array(
+    "x-api-key: 7a0ba247-ff23-4c94-b982-fe1c696d1051"
+  ),
 ));
 
 $response = curl_exec($curl);
