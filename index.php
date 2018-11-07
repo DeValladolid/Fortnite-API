a9e6fb90a255aeb556df0f995825a1daac799b6a
DeValladolid <devalladolidoficial@gmail.com>
2018-11-07 12:31:36 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,37 +1,4 @@
 <?php
-
-$curl = curl_init();
-
-curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://fortnite-public-api.theapinetwork.com/prod09/store/get",
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => "",
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 30,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS => "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"language\"\r\n\r\nen\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--",
-  CURLOPT_HTTPHEADER => array(
-    "Authorization: 5ae9fa892ff9cf7b4bfb2cda585bc77a",
-    "content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW"
-  ),
-));
-
-$err = curl_error($curl);
-
-curl_close($curl);
-
-if ($err) {
-  echo "cURL Error #:" . $err;
-} else {
-  echo $response;
-}
-
-var_dump(json_decode($response));
-var_dump(json_decode($json, true));
-
-?>
-<?php
 require_once 'fortnite-api/Autoloader.php';
 
 $api = new FortniteClient;
@@ -41,6 +8,3 @@ $data = $api->items->store();
 
 var_dump($data);
 ?>
-
-
-
