756eb8eae852d07401bf1e1e8f61843a5034cb92
DeValladolid <devalladolidoficial@gmail.com>
2018-11-07 10:58:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,19 +3,19 @@
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://fnbr.co/api/shop",
+  CURLOPT_URL => "https://fortnite-public-api.theapinetwork.com/prod09/store/get",
   CURLOPT_RETURNTRANSFER => true,
   CURLOPT_ENCODING => "",
   CURLOPT_MAXREDIRS => 10,
   CURLOPT_TIMEOUT => 30,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"language\"\r\n\r\nen\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--",
   CURLOPT_HTTPHEADER => array(
-    "x-api-key: 7a0ba247-ff23-4c94-b982-fe1c696d1051"
+    "Authorization: 5ae9fa892ff9cf7b4bfb2cda585bc77a",
+    "content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW"
   ),
 ));
-
-$data = $api->items->store();
 
 $response = curl_exec($curl);
 $err = curl_error($curl);
