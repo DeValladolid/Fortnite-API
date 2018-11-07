1159de0d0d8466f5a0438c42dac8f4a9e30024bd
DeValladolid <devalladolidoficial@gmail.com>
2018-11-07 10:52:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,29 @@
 <?php
-require_once 'fortnite-api/Autoloader.php';
+<?php
 
-$api = new FortniteClient;
-$api->setKey('5ae9fa892ff9cf7b4bfb2cda585bc77a');
+$curl = curl_init();
 
-$data = $api->items->store();
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://fnbr.co/api/shop",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => array(
+    "x-api-key: 7a0ba247-ff23-4c94-b982-fe1c696d1051"
+  ),
+));
 
-var_dump($data);
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
 ?>
